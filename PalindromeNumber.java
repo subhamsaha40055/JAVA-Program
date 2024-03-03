4519c3919cebc9fbd73443de264f0e84a23269e5
SUBHAM SAHA <160627150+subhamsaha40055@users.noreply.github.com>
2024-03-03 21:23:23 +0530

    Add files via upload
    //Palindrome Number
    Output
    Enter number to check palindrome or not:121
    121 is palindrome.
    Enter number to check palindrome or not:523 
    325 is not palindrome.

---
+++ PalindromeNumber.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+class Number
+{
+    int s,r,k;
+    void fn(int p)
+    {
+        k=p;
+        s=0;
+        while(p!=0)
+        {
+          r=p%10;
+          s=s*10+r;
+          p=p/10;
+        }
+    }
+    void fn()
+    {
+        if(k==s)
+        System.out.println(s+" is palindrome");
+        else
+        System.out.print(s+" is not palindrome");
+    }
+}
+class PalindromeNumber
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number to cheak palimdrome or not:");
+        n=sc.nextInt();
+        Number obj = new Number();
+        obj.fn(n);
+        obj.fn();
+
+    }
+}
